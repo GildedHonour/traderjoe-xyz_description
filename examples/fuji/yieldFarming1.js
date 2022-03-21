d28118a6c3c4a33b932b4f843d3ac6b9d5ed9c70
alex maslakov <alex@serendipia.email>
2022-03-21 07:29:45 -0300

    wip

--- examples/fuji/yieldFarming1.js
+++ examples/fuji/yieldFarming1.js
@@ -3,7 +3,7 @@ import * as utilities from './utilities.js'
 
 const ADD_LIQUIDITY_FUNC_ABI = 'function addLiquidity(address tokenA, address tokenB, uint amountADesired, uint amountBDesired, uint amountAMin, uint amountBMin, address to, uint deadline) external returns (uint amountA, uint amountB, uint liquidity)';
 const ADD_LIQUIDITY_AVAX_FUNC_ABI = 'function addLiquidityAVAX(address token, uint256 amountTokenDesired, uint256 amountTokenMin, uint256 amountAVAXMin, address to, uint256 deadline)';
-const LP_ID = 123; //TODO - does it have to be found out from a DEX?
+const LP_ID = 123; //TODO: подставить нужный нам id
 
 async function main() {
   const AVAXAmount = ethers.utils.parseEther('0.000001').toHexString();
@@ -25,13 +25,9 @@ async function main() {
     utilities.account
   );
 
-
-  //
-  //TODO
-  //
-
   const lpt0 = new ethers.Contract(
-      //for simplicity ; instead it should be Joe LP token (JoePair)
+      //используем WAVAX для простоты;
+      //в реальном же проэкте - использовать реальный LP-токен
       utilities.addresses.WAVAX, 
       [
         ERC20_ABI
@@ -40,17 +36,25 @@ async function main() {
   );
   await utilities.approveERC20Token(lpt0, router.address, largeAmount);
 
-  const deposAmount = ethers.utils.parseUnits('0.001', 'gwei');
-  
-  //предварительно добавить WAVAX в LP
-  // const tx = await mcv3.add(1, lpt0, utilities.addresses.zero);
-  const tx = await mcv3.deposit(
-    LP_ID, //TODO
-    deposAmount
+  //только для админа
+  //добавить и настроить вознограждения для определённого LP-токена
+  //вызвать обязательно при первичной настройке, один раз для каждого LP токена;
+  const tx = await mcv3.add(
+    100,
+    utilities.addresses.WAVAX,
+    utilities.addresses.zero
   );
 
 
+  //дезопизит LP-токена
+  const deposAmount = ethers.utils.parseUnits('0.001', 'gwei');
+  const tx = await mcv3.deposit(
+    LP_ID,
+    deposAmount
+  );
 
+  //NOTE
+  //amountLeftWithoutNeedForApproval = largeAmount - deposAmount
 }
 
 main()
